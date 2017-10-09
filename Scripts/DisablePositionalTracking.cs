bd7e80d4a3f39188fe172e8262f885772c43f3de
justvdlinde <justvdlinde@hotmail.com>
2017-10-09 13:05:00 +0200

    Add files via upload

---
+++ Scripts/DisablePositionalTracking.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisablePositionalTracking : MonoBehaviour {
+
+	public GameObject cameraGameObject;
+
+	void Start(){
+		cameraGameObject = Camera.main.gameObject;
+	}
+
+	// Disable positional tracking by applying an opposite offset from the camera that's being
+	// moved by the HMD. This script should be applied on an empty parent object of the camera.
+	void Update ()
+	{
+		if (cameraGameObject != null)
+		{
+			Vector3 offset = new Vector3(
+				-cameraGameObject.transform.localPosition.x,
+				-cameraGameObject.transform.localPosition.y, 
+				-cameraGameObject.transform.localPosition.z);
+			transform.localPosition = offset;
+		}
+	}
+}
