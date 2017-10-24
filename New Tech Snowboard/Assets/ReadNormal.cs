cbe571a50a36606fc5ac68bc855afe577021c438
Celine de Wijs <31764854+CdeWijs@users.noreply.github.com>
2017-10-24 18:18:45 +0200

    Upload Unity scene

---
+++ New Tech Snowboard/Assets/ReadNormal.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReadNormal : MonoBehaviour {
+
+    RaycastHit hit;
+    Ray ray = new Ray(new Vector3(0,0,0), Vector3.down);
+    float step;
+	
+	// Update is called once per frame
+	void Update () {
+        step = 100 * Time.deltaTime;
+		if(Physics.Raycast(ray, out hit)) {
+            if(hit.collider.tag == "Terrain") {
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(hit.normal), step);
+            }
+        }
+	}
+}
