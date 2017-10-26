93c78aa0805718ff97f5cbff18a6fb602821d0b0
Celine de Wijs <31764854+CdeWijs@users.noreply.github.com>
2017-10-26 13:20:28 +0200

    Upload Scene v2

---
+++ New Tech Snowboard/Assets/Collectible.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour {
+
+    private AudioSource audioSource;
+
+    private void Start() {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        audioSource.Play();
+        Invoke("DestroyObject", 0.2f);
+    }
+
+    private void DestroyObject() {
+        Destroy(gameObject);
+    }
+}
